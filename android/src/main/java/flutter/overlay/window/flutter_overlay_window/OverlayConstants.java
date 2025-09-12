357554e9fe2a9bf955813c98bacffb39538a9737
Hamza Amjad <139040504+11hamzaha@users.noreply.github.com>
2025-09-12 09:31:01 +0500

    Update OverlayConstants.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayConstants.java
@@ -5,7 +5,13 @@ final public class OverlayConstants {
     static final String CACHED_TAG = "myCachedEngine";
     static final String CHANNEL_TAG = "x-slayer/overlay_channel";
     static final String OVERLAY_TAG = "x-slayer/overlay";
+    
+    // Original messenger tag for main app -> overlay communication
     static final String MESSENGER_TAG = "x-slayer/overlay_messenger";
+    
+    // NEW: Messenger tag for overlay -> main app communication
+    static final String MAIN_APP_MESSENGER_TAG = "x-slayer/main_app_messenger";
+    
     static final String CHANNEL_ID = "Overlay Channel";
     static final int NOTIFICATION_ID = 4579;
     static final int DEFAULT_XY = -6;
